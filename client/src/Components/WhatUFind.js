9d3f1d52c53fbe4fb524e323604ae04b5ae70c52
Dominik <dominik.glegola127@gmail.com>
2022-07-06 13:42:23 +0200

    hot_fix_v6

--- client/src/Components/WhatUFind.js
+++ client/src/Components/WhatUFind.js
@@ -10,7 +10,19 @@ const WhatUFind = (props) => {
                    <h1 className='header fw-bold'>{t('whatufind.head')}</h1>
                 </div>
                 <div className='row '>
-                    <p className="mb-3">{t('whatufind.paragraf.first')}</p>
+                    <p className="mb-3">
+                        {t('whatufind.paragraf.first.1')}
+                        <b>{t('whatufind.paragraf.first.2')}</b>
+                        {t('whatufind.paragraf.first.3')}
+                        <b>{t('whatufind.paragraf.first.4')}</b>
+                        {t('whatufind.paragraf.first.5')}
+                        <b>{t('whatufind.paragraf.first.6')}</b>
+                        {t('whatufind.paragraf.first.7')}
+                        <b>{t('whatufind.paragraf.first.8')}</b>
+                        {t('whatufind.paragraf.first.9')}
+                        <b>{t('whatufind.paragraf.first.10')}</b>
+                        {t('whatufind.paragraf.first.11')}
+                    </p>
                     <p className="my-0">{t('whatufind.paragraf.second')}</p>
                     <p>{t('whatufind.paragraf.third')}</p>   
                 </div>
